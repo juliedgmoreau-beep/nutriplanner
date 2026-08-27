38db017929d052975bd68b95b679b831f6b1f8e5
juliedgmoreau-beep <juliedg.moreau@gmail.com>
2026-08-27 14:12:35 +0000

    Updates

--- _next/static/kWChYTq0KmxtrX5y7c4Vw/_buildManifest.js
+++ _next/static/kWChYTq0KmxtrX5y7c4Vw/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nutriplanner//_next/:path+",
+        "destination": "/nutriplanner/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
